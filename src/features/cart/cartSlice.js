dc66b6226bdd82196a99eb551cebf6922f14c3bf
Ivan Borisov <i.borisov12@outlook.com>
2025-07-29 19:56:57 +0300

    Added the project

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    removeItem(state, action) {
+      state.cart = state.cart.filter(
+        (pizza) => pizza.pizzaId !== action.payload
+      );
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((pizza) => pizza.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((pizza) => pizza.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  removeItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+// export function getTotalCartQuantity(store) {
+//   store.cart.cart.reduce((acc, item) => acc + item.quantity, 0);
+// }
+
+// export function getTotalPrice(store) {
+//   store.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
+// }
+
+export const getTotalCartQuantity = (store) =>
+  store.cart.cart.reduce((acc, item) => acc + item.quantity, 0);
+
+export const getTotalPrice = (store) =>
+  store.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
